1aad27536515dfbf3d48ed4899b21ec99d8c1b59
John McKie <therealjmac2097@gmail.com>
2018-07-18 21:30:05 +0100

    Basioc server setup with some sass going in.  Issue getting homepage up though

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Path Variables
+const HTML_PATH = 'src/*.html';
+// const CSS_PATH = 'src/css/**/*.css';
+const SCSS_PATH = 'src/scss/*.scss';
+
+
+// Compile sass
+gulp.task('sass', function() {
+    return gulp.src(SCSS_PATH)
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream())
+});
+
+
+// Watch And Serve
+gulp.task('serve',  gulp.parallel('sass', function() {
+    browserSync.init({
+        server: '/src'
+    });
+
+    gulp.watch(SCSS_PATH, gulp.parallel('sass'));
+    gulp.watch(HTML_PATH).on('change', browserSync.reload);
+}));
+
+// Default
+gulp.task('default', gulp.parallel('serve'));
