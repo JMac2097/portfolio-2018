c094524e89ad3f0836ba70322fa21567e95921fa
John McKie <therealjmac2097@gmail.com>
2018-07-23 21:24:29 +0100

    Another new layout from css.tricks

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,73 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const newer = require('gulp-newer');
-const imagemin = require('gulp-imagemin');
-const htmlclean = require('gulp-htmlclean');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
 
-// folders
-const folder = {
-    src: 'src/',
-    build: 'build/'
-}
-
-// Path Variables
-const SCSS_PATH = '/src/scss/*.scss';
-const SCRIPTS_PATH = '/src/scripts/*.js';
-
-gulp.task('html', ['sass','images'], () => {
-    let out = folder.build + 'html/';
-        gulp.src(folder.src + 'html/**/*')
-        .pipe(newer(out))
-        .pipe(htmlclean())
-        .pipe(gulp.dest(out))        
-});
-
-// Compile sass
-gulp.task('sass', () => 
-    gulp.src(SCSS_PATH)
+// sass task
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('build/css'))
-    .pipe(browserSync.stream())
-);
-
-// Babel ES6 
-gulp.task('scripts', () => 
-    gulp.src(SCRIPTS_PATH)
-    .pipe(babel({
-        presets: ['env']
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+        stream: true
     }))
-    .pipe(gulp.dest('public/scripts'))
-);
-
-// images task
-gulp.task('images', () => {
-    let out = folder.build + 'images/';
-    gulp.src(folder.src + 'images/**/*')
-        .pipe(newer(out))
-        .pipe(imagemin({ optimizationLevel: 5 }))
-        .pipe(gulp.dest(out));
 });
 
-// Serve
-gulp.task('serve', () => 
+// image minification
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin({
+        interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// fonts task
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+});
+
+// clean distribution
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+// clean image cache
+gulp.task('cache:clear', function(callback) {
+    return cache.clearAll(callback)
+});
+
+// useref task
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+    .pipe(useref())
+    // minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+    //minifies if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+// serve
+gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: "./build/html"
-        }
+            baseDir: 'app'
+        },
     })
-);
+});
 
-// Default
-gulp.task('default', ['sass','images','html','scripts','serve']);
+// watch
+gulp.task('watch', ['serve', 'sass'], function(){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
