89a9973a433ecd2a2750ea48cf9c9a5376b45134
John McKie <therealjmac2097@gmail.com>
2018-07-30 20:30:15 +0100

    Some proto joavascript background image stuff

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,12 @@
 /*jshint esversion: 6 */ 
 
+let showHomeBackground = true;
+const body = document.body;
+
+document.addEventListener('DOMContentLoaded', checkForHomeBackground);
+
+function checkForHomeBackground() {
+    if(showHomeBackground) {
+        body.classList.add("bg-image");
+    }
+};
